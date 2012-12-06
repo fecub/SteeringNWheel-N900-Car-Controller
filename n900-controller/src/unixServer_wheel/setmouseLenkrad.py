510cb1bfc321ac1524ab420345fd6d8903e08e07
fecub <fcbk67@gmail.com>
2012-12-06 10:47:50 +0100

    anpassungen für android und maemo.
    Läuft jetzt mit beiden Geräten

--- n900-controller/src/unixServer_wheel/setmouseLenkrad.py
+++ n900-controller/src/unixServer_wheel/setmouseLenkrad.py
@@ -4,39 +4,47 @@ Created on 12.03.2011
 @author: fecub
 '''
 
-from Xlib import X, display
-import socket, sys, time
+from Xlib import display
+import socket, time
+import math
+import subprocess
 
+
+screen_size = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f4',shell=True, stdout=subprocess.PIPE).communicate()[0]
+screen_size = screen_size.split("x")
+screen_size_x = int(screen_size[0])
+screen_size_x_half = screen_size_x / 2
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-try:
-	s.bind(("", 50000))
-	x1 = 0
-	y1 = 0
+s.bind(("", 50000))
+x1 = 0
+y1 = 0
 
-	d = display.Display()
-	screen = d.screen()
-	root = screen.root
+d = display.Display()
+screen = d.screen()
+root = screen.root
 
-	while True:
-		time.sleep(0.1)
-		daten, addr = s.recvfrom(1024)
-		
-		coord = daten
-		coord=coord.split(',')
+android = True
+
+while True:
+	time.sleep(0.1)
+	daten, addr = s.recvfrom(1024)
+	#print daten
+	coord = daten
+	coord=coord.split(',')
+	
+	if (android):
+		x = float(coord[1])
+		y = float(coord[0])
+		pointer = int(math.floor(float(((x/3)*210) + screen_size_x_half)))
+	else:
 		x = int(coord[1])
 		y = int(coord[0])
-		
-		#print x
-		y+=640
-		#y*=-1
-		
-		root.warp_pointer(y,300)
-		d.sync()
 
-finally:
-	s.close()
+		pointer = y + screen_size_x_half
 	
-	
-	
+	print pointer
+	root.warp_pointer(pointer,300)
+	d.sync()
+s.close()
